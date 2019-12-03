8528b6ed8ed552a2866f56ed97b17285c264a4ef
Benjamin <benjamin.willems@student.vinci.be>
2019-12-03 10:03:58 +0100

    ajout du projet vide

---
+++ ESOPE/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ESOPE::Application.config.secret_token = '010fedb656f1e3a5aa1d1327c64df4af1e23664f13e64686c8daf218affe8ff62deb2aa3e9885818c01e262859ff0d8132012423be13ab4ac12de4079cf63973'
