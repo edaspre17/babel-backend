dde1592b44e2682775242613e58808160439dc85
Benjamin <benjamin.willems@student.vinci.be>
2019-12-06 10:19:05 +0100

    oups

--- esope_backend/test/controllers/connection_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ConnectionControllerTest < ActionDispatch::IntegrationTest
-  test "should get connect" do
-    get connection_connect_url
-    assert_response :success
-  end
-
-end
